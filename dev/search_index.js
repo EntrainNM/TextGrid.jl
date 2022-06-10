fc0f7c75a04d1e60b9314107fdc58ef7f240e814
Documenter.jl <documenter@juliadocs.github.io>
2022-06-10 06:43:05 +0000

    build based on 2799ba9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TextGrid.jl","page":"TextGrid.jl","title":"TextGrid.jl","text":"","category":"section"},{"location":"","page":"TextGrid.jl","title":"TextGrid.jl","text":"Documentation for TextGrid.jl","category":"page"}]
+}
